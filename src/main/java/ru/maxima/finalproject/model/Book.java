8245fad73b762954144ff5498f5360547aa43364
AramaJava <abr-aram@yandex.ru>
2023-08-10 00:16:41 +0500

    first init

---
+++ src/main/java/ru/maxima/finalproject/model/Book.java
@@ -0,0 +1,33 @@
+package ru.maxima.finalproject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author AramaJava 09.08.2023
+ */
+@Entity
+@Data
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+    private String name;
+    private Integer yearOfProduction;
+    private String  author;
+    private String annotation;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime removedAt;
+    private String createdPerson;
+    private String updatedPerson;
+    private String removedPerson;
+    @ManyToOne
+    @JoinColumn(name="person_id", referencedColumnName = "id")
+    private Person owner;
+}
---
+++ src/main/java/ru/maxima/finalproject/model/Person.java
@@ -0,0 +1,35 @@
+package ru.maxima.finalproject.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author AramaJava 09.08.2023
+ */
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="person_id")
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime removedAt;
+    private String createdPerson;
+    private String removedPerson;
+    @OneToMany(mappedBy = "owner")
+    private List<Book> takenBooks;
+}
