2d7187fcfd47cb899c334a7e4e9391da4e3bd082
AramaJava <abr-aram@yandex.ru>
2023-08-10 14:07:34 +0500

    first init

---
+++ src/main/java/ru/maxima/finalproject/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package ru.maxima.finalproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.maxima.finalproject.model.Book;
+
+/**
+ * @author AramaJava 10.08.2023
+ */
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
