5ec537f5908b916905ea7f6f7aef671ee5d04bd1
AramaJava <abr-aram@yandex.ru>
2023-08-16 13:32:24 +0500

    admin add script

---
+++ src/main/resources/data/add_admin_first.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.person (age, createdat, createdperson, email, name,
+                           password, phonenumber, removedat, removedperson, role)
+VALUES (42, '2014-04-22 00:00:00.000000', '2014-04-22 00:00:00', 'admin@mail.ru', 'Anton', '$2a$12$e/tKvTxyNSmuit6YJMudc.VPymwM3x6s3GkTNZNel.kQv5qd6DqFq', '+7895434534', '2014-04-22 00:00:00.000000', '2014-04-22 00:00:00', 'admin');
+
