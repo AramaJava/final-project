f4d8c404ed7c6da6437461a3af95386591cd5ada
AramaJava <abr-aram@yandex.ru>
2023-08-16 23:21:42 +0500

    admin add script

--- src/main/resources/data/add_admin_first.sql
+++ src/main/resources/data/add_admin_first.sql
@@ -1,4 +1,6 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 INSERT INTO public.person (age, createdat, createdperson, email, name,
                            password, phonenumber, removedat, removedperson, role)
-VALUES (42, '2014-04-22 00:00:00.000000', '2014-04-22 00:00:00', 'admin@mail.ru', 'Anton', '$2a$12$e/tKvTxyNSmuit6YJMudc.VPymwM3x6s3GkTNZNel.kQv5qd6DqFq', '+7895434534', '2014-04-22 00:00:00.000000', '2014-04-22 00:00:00', 'admin');
+VALUES (42, '2014-04-22 00:00:00.000000', '2014-04-22 00:00:00', 'admin@mail.ru', 'Anton', crypt ('2222', gen_salt('bf', 12) ) , '+7895434534', '2014-04-22 00:00:00.000000', '2014-04-22 00:00:00', 'admin');
 
