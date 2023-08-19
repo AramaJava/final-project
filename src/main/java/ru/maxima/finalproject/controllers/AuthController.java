82abb5ab5094e46fd2850f4527bf49e78dcf9c29
AramaJava <abr-aram@yandex.ru>
2023-08-19 10:17:29 +0500

    add service impl

--- src/main/java/ru/maxima/finalproject/controllers/AuthController.java
+++ src/main/java/ru/maxima/finalproject/controllers/AuthController.java
@@ -1,7 +1,6 @@
 package ru.maxima.finalproject.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.maxima.finalproject.services.AuthService;
 import ru.maxima.finalproject.models.Person;
@@ -12,12 +11,6 @@ import ru.maxima.finalproject.models.Person;
 public class AuthController {
 
     private final AuthService authService;
-
-    @PostMapping("/reg/{adminId}")
-    @PreAuthorize("hasAnyAuthority(@authorities.ROLE_ADMIN)")
-    public void registration(@RequestBody Person user, @PathVariable Long adminId) {
-        authService.registration(user, adminId);
-    }
 
     @GetMapping("/login")
     public String authentication(@RequestBody Person person) {
